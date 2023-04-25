3b98d5bd1d1be19b3ce544a3933e6bfda2af5bb8
SekharAdhikarla <Adhikarlavenkatasekhar@gmail.com>
2023-04-25 00:38:03 +0000

    commit message

---
+++ myapp/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+  let date = new Date();
+
+  response.send(`"Today" date ${date}`);
+});
+
+app.get("/page", (request, response) => {
+  response.sendFile("./page.html", { root: __dirname });
+});
+app.listen(3000);
